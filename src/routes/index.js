ae9e2b6f5a6295810d74b62ba4989c4cede2c186
lDanielZasso <daniel.zasso@usach.cl>
2022-07-03 00:46:17 -0400

    Primer Commit

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.render('index', {title: 'First Website with Node'})
+})
+
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+export default router
